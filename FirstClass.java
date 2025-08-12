7033474899403ab5d361db20c458c1075a986a9e
Dikshit <gargdikshit22@gmail.com>
2025-08-12 18:38:52 +0530

    Done

--- FirstClass.java
+++ FirstClass.java
@@ -1,7 +1,8 @@
 class FirstClass{
-    public static void main(String arg[]){
+    public static void main(String[] arg){
         System.out.println("Hello World");
         System.out.println("My Nmae is Dikshit");
         System.out.println("I am learning Java");
+
     }
 }
